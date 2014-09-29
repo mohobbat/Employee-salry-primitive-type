1efe042da19bab2343c5c4aa96994a706242cd9d
mohobbat <raihan_ice07@yahoo.com>
2014-09-29 15:45:06 +0600

    added

---
+++ EmploySalaryPrimitiveApp/EmploySalaryPrimitiveApp/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace EmploySalaryPrimitiveApp
+{
+    partial class EmpleySalaryApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.retriveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(19, 35);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 0;
+            this.ID.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salary";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(107, 35);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(183, 20);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(107, 92);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(107, 167);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(183, 20);
+            this.salaryTextBox.TabIndex = 5;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(60, 268);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // retriveButton
+            // 
+            this.retriveButton.Location = new System.Drawing.Point(203, 268);
+            this.retriveButton.Name = "retriveButton";
+            this.retriveButton.Size = new System.Drawing.Size(75, 23);
+            this.retriveButton.TabIndex = 7;
+            this.retriveButton.Text = "Retrive";
+            this.retriveButton.UseVisualStyleBackColor = true;
+            this.retriveButton.Click += new System.EventHandler(this.retriveButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(342, 268);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // EmpleySalaryApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 334);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.retriveButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ID);
+            this.Name = "EmpleySalaryApp";
+            this.Text = "EmploySalaryUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button retriveButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ EmploySalaryPrimitiveApp/EmploySalaryPrimitiveApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmploySalaryPrimitiveApp
+{
+    public partial class EmpleySalaryApp : Form
+    {
+        public EmpleySalaryApp()
+        {
+            InitializeComponent();
+        }
+
+        private string id;
+        private string name;
+        private double salary;
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            id = idTextBox.Text;
+            name = nameTextBox.Text;
+            salary = Convert.ToDouble(salaryTextBox.Text);
+
+            string mesg = "id" + " " + id + "\nname" + " " + name + "\nsalary" + salary;
+
+            MessageBox.Show(mesg);
+
+        }
+
+        private void retriveButton_Click(object sender, EventArgs e)
+        {
+            idTextBox.Text = id;
+            nameTextBox.Text = name;
+            salaryTextBox.Text = salary.ToString();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+            idTextBox.Text = string.Empty;
+            nameTextBox.Text = string.Empty;
+            salaryTextBox.Text = string.Empty;
+        }
+    }
+}
